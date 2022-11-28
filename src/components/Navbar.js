7134cb39218a72e05c4900a51dc4a96a807ed0f7
anil konda <I11200049@in.techwave.net>
2022-11-28 21:14:52 +0530

    social media mui

---
+++ src/components/Navbar.js
@@ -0,0 +1,109 @@
+import { Notifications, Pets } from '@mui/icons-material';
+import MailIcon from '@mui/icons-material/Mail';
+import {
+  AppBar,
+  Avatar,
+  Badge,
+  Box,
+  InputBase,
+  Menu,
+  MenuItem,
+  styled,
+  Toolbar,
+  Typography,
+} from '@mui/material';
+import React, { useState } from 'react';
+
+const StyledToolbar = styled(Toolbar)({
+  display: 'flex',
+  justifyContent: 'space-between',
+});
+
+const Search = styled('div')(({ theme }) => ({
+  backgroundColor: 'white',
+  padding: '0 10px',
+  borderRadius: theme.shape.borderRadius,
+  width: '40%',
+}));
+
+// breakpoints.up('sm') means sm>600px below properties we defined will be applicable
+
+// Here if the sm>600px only we are showing the icons of mail n notifications,
+// if sm<600px then we are displaying it as none
+const Icons = styled(Box)(({ theme }) => ({
+  display: 'none',
+  alignItems: 'center',
+  gap: '20px',
+  [theme.breakpoints.up('sm')]: {
+    display: 'flex',
+  },
+}));
+
+// Here if the breakpoint is more than the sm like > 600px then we are showing nothing
+// if sm<600px then we are showing the avatar with the user name
+const UserBox = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  gap: '10px',
+  [theme.breakpoints.up('sm')]: {
+    display: 'none',
+  },
+}));
+
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
+  return (
+    <AppBar position='sticky'>
+      <StyledToolbar>
+        <Typography variant='h6' sx={{ display: { xs: 'none', sm: 'block' } }}>
+          Media
+        </Typography>
+        <Pets sx={{ display: { xs: 'block', sm: 'none' } }} />
+        <Search>
+          <InputBase placeholder='search...' />
+        </Search>
+        <Icons>
+          <Badge badgeContent={4} color='error'>
+            <MailIcon />
+          </Badge>
+          <Badge badgeContent={4} color='error'>
+            <Notifications />
+          </Badge>
+          <Avatar
+            sx={{ width: 30, height: 30 }}
+            src='https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+            onClick={(e) => setOpen(true)}
+          />
+        </Icons>
+        <UserBox onClick={(e) => setOpen(true)}>
+          <Avatar
+            sx={{ width: 30, height: 30 }}
+            src='https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+          />
+          <Typography variant='span'>Anil Konda</Typography>
+        </UserBox>
+      </StyledToolbar>
+
+      <Menu
+        id='demo-positioned-menu'
+        aria-labelledby='demo-positioned-button'
+        open={open}
+        onClose={(e) => setOpen(false)}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+      >
+        <MenuItem>Profile</MenuItem>
+        <MenuItem>My account</MenuItem>
+        <MenuItem>Logout</MenuItem>
+      </Menu>
+    </AppBar>
+  );
+};
+
+export default Navbar;
